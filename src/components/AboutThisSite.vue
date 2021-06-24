8479cf1903507db9884beb33f7d2e7f28b8c7858
Nicholas Bennett <nicholas.bennett@kellogg.northwestern.edu>
2021-06-24 11:29:27 -0500

    Fix spelling error on About page.

--- src/components/AboutThisSite.vue
+++ src/components/AboutThisSite.vue
@@ -11,7 +11,7 @@
             <v-col>
                 <p>This site is built using <a href="https://vuejs.org/">Vue.js</a> and associated libraries, the same stack used for the <router-link to="/howitsmade">proto-payoff-client</router-link>.</p>
                 <p>This app is published to the web for free using <a href="https://surge.sh/">surge.sh</a>, which is a CDN as well as a command-line tool (installed via Node's <a href="https://docs.npmjs.com/about-npm">npm</a> tool).</p>
-                <p>Code for this site is <a href="https://github.com/lovingawareness/metaprotopayoff">publically available</a>.</p>
+                <p>Code for this site is <a href="https://github.com/lovingawareness/metaprotopayoff">publicly available</a>.</p>
                 <p>Content initially drafted on <a href="https://slides.com">slides.com</a>.</p>
                 <p>Made by <a href="https://github.com/lovingawareness">Nicholas Bennett</a> for <a href="https://www.kellogg.northwestern.edu/">Kellogg School of Management</a> at <a href="https://northwestern.edu">Northwestern University</a>.</p>
             </v-col>
